e1d690f2470430c4d9de188c1ef8101e65fee28b
Lirian Su <l@cloudisdream.com>
2015-04-20 00:50:08 +0800

    Add Sqlite database

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+import sqlite3 as s
 
 from tornado.options import define, options
 
@@ -11,15 +13,23 @@ define("port", default=8888, help="run on the given port", type=int)
 
 class MainPageHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("index.html", error=None);
+        self.render("index.html");
 
 class ContactHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("contact.html", error=None);
+        self.render("contact.html");
 
 class PlaceHandler(tornado.web.RequestHandler):
     def get(self, pathname):
-        self.write(pathname)
+        cx = s.connect("sqlite.db")
+        cu = cx.cursor()
+        select_sql = "select name from place where url = ?"
+        cu.execute(select_sql, [pathname])
+        name = cu.fetchone()
+        if name:
+            self.render("place.html", path=pathname, name=name[0])
+        else:
+            self.render("place.html", path=pathname, name="无页面")
 
 def main():
     tornado.options.parse_command_line()
---
+++ schema/sqlite.sql
@@ -0,0 +1,8 @@
+drop table if exists place;
+
+create table place(
+    id   int           auto_increment,
+    url  varchar(20),
+    name varchar(40),
+    img  varchar (60)
+);
