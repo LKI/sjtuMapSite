0603f5e71e31befe5395cb035179454db8ecc882
Lirian Su <l@cloudisdream.com>
2015-06-03 15:11:42 +0800

    Add places and update schema

--- schema/sqlite.sql
+++ schema/sqlite.sql
@@ -1,8 +1,17 @@
-drop table if exists place;
+--dbext:profile=sqlite
 
-create table place(
-    id   int           auto_increment,
-    url  varchar(20),
-    name varchar(40),
-    img  varchar (60)
+drop table if exists place;
+create table place (
+    id      int            auto_increment,
+    url     varchar(20),
+    name    varchar(40),
+    img     varchar(60)
+);
+
+drop table if exists story;
+create table story (
+    id      int             auto_increment,
+    p_id    int,
+    content varchar(1000),
+    avt     varchar(60)
 );
